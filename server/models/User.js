f2166fdd74027964fa084fb51a3a9d650c7ae04f
Kazniyaz Kabyldenov <kazniyazkz@gmail.com>
2018-10-24 20:17:29 -0400

    Added User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,36 @@
 const conn = require('./conn');
+const Sequelize = require('sequelize');
 
-const User = '';
+const User = conn.define('users', {
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    required: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    required: true,
+    validate: {
+      is: ['^[a-z]+$', 'i'],
+    },
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    required: true,
+    validate: {
+      is: ['^[a-z]+$', 'i'],
+    },
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    required: true,
+  },
+});
 
 module.exports = User;
