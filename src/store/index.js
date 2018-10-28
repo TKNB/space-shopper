e32d3f48ed9ef50744588e6440560f7014792bb6
tom-licitra <38163713+tom-licitra@users.noreply.github.com>
2018-10-28 12:16:56 -0400

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,12 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
-import tempAuthReducer from './tempAuth';
 import usersReducer from './users';
 import productsReducer from './products';
 import ordersReducer from './orders';
 import authReducer from './auth';
 
 const reducer = combineReducers({
-  auth: tempAuthReducer,
   users: usersReducer,
   products: productsReducer,
   orders: ordersReducer,
