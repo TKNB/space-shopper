3d396283612904c0b081b1d6c94ae2f93ab65912
Kazniyaz Kabyldenov <kazniyazkz@gmail.com>
2018-11-03 10:55:34 -0400

    Add rest of the routes to reviews

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -5,6 +5,7 @@ const User = require('../models/User');
 
 //API
 //http://localhost:8888/api/reviews
+
 router.get('/', (req, res, next) => {
   Review.findAll()
     .then(reviews => res.send(reviews))
@@ -18,6 +19,29 @@ router.get('/:productId', (req, res, next) => {
     },
   })
     .then(reviews => res.send(reviews))
+    .catch(next);
+});
+
+// Create a new review
+router.post('/', (req, res, next) => {
+  Review.create(req.body)
+    .then(review => res.send(review))
+    .catch(next);
+});
+
+// Update a review
+router.put('/:id', (req, res, next) => {
+  Review.findById(req.params.id)
+    .then(review => review.update(req.body))
+    .then(review => res.send(review))
+    .catch(next);
+});
+
+// Delete a review
+router.delete('/:id', (req, res, next) => {
+  Review.findById(req.params.id)
+    .then(review => review.destroy())
+    .then(() => res.sendStatus(204))
     .catch(next);
 });
 
