8eae558339b1eadbc9786b2eee83c51cceb29a04
Nicholas Melhado <n.melhado@gmail.com>
2018-11-04 10:16:10 -0500

    added favicons

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use('/api', require('./routes'));
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/favicons', express.static(path.join(__dirname, '..', 'public/favicons')));
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'));
